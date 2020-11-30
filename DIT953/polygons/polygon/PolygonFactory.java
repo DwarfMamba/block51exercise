8d55e643b6ea0900718a7ead91324d1d37974374
Oscar Palmqvist <oscar@palmqvist.eu>
2020-11-30 10:40:48 +0100

    cvbbd

---
+++ DIT953/polygons/polygon/PolygonFactory.java
@@ -0,0 +1,13 @@
+package DIT953.polygons.polygon;
+
+public class PolygonFactory<T extends Polygon> {
+
+    public T rectangleFactory<T>(){
+        System.out.println("Hej");
+    }
+    
+
+
+
+
+}
