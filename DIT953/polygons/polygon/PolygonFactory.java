c942c6d62dce22d107110aef2cfece54d7233d76
Oscar Palmqvist <oscar@palmqvist.eu>
2020-11-30 10:49:57 +0100

    asdfasd

--- DIT953/polygons/polygon/PolygonFactory.java
+++ DIT953/polygons/polygon/PolygonFactory.java
@@ -1,11 +1,21 @@
 package DIT953.polygons.polygon;
 
-public class PolygonFactory<T extends Polygon> {
+public class PolygonFactory {
 
-    public T rectangleFactory<T>(){
-        System.out.println("Hej");
+    public static Rectangle createRectangle(int x,int y){
+        Rectangle newObj = new Rectangle(x,y);
+        return newObj;
     }
-    
+    public static Square createSquare(int x,int y){
+        Square newObj = new Square(x,y);
+        return newObj;
+    }
+    public static Triangle createTriangle(int x,int y){
+        Triangle newObj = new Triangle(x,y);
+        return newObj;
+    }
+
+
 
 
 
